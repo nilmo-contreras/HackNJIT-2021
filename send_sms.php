1e3a7a5368b62437506fffc4fc54d0c417b01e67
ncontreras752 <77853767+nilmo-contreras@users.noreply.github.com>
2021-11-14 00:33:03 -0500

    added sms environmental variables

--- send_sms.php
+++ send_sms.php
@@ -2,21 +2,23 @@
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
+// Load our environment variables from the .env file:
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$auth_token = 'your_auth_token';
 // In production, these should be environment variables. E.g.:
-// $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
+$account_sid = $_ENV['TWILIO_ACCOUNT_SID'];
+$auth_token = $_ENV['TWILIO_AUTH_TOKEN'];
 
 // A Twilio number you own with SMS capabilities
-$twilio_number = "+15017122661";
+$twilio_number = "+18553665130";
 
 $client = new Client($account_sid, $auth_token);
 $client->messages->create(
     // Where to send a text message (your cell phone?)
-    '+15558675310',
+    '+19733321820',
     array(
         'from' => $twilio_number,
-        'body' => 'I sent this message in under 10 minutes!'
+        'body' => 'Test!'
     )
 );
